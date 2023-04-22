2560af88fdd61e0dca1ed0308540ad2e0326c12f
kakamaran <knappe.josef@web.de>
2023-04-22 11:05:54 +0200

    autotable mit inital state

--- src/fsm.c
+++ src/fsm.c
@@ -10,18 +10,18 @@ static enum {
 // manual operation mode
 ////////////////////////////////////////////////////////////////////////////////
 typedef enum{ 		// TODO: define states for manual mode here
-	ruhe, oeffnen, schließen, fehler, // TODO: add states here
-	Manu_Initial // = Geschlossen
+	ruhe, schliessen, oeffnen // = Geschlossen
 } ManuState_t;
 
 
 typedef union {// TODO: define an input type for manual mode here
 
 	struct {		 //brauchen wir da lb? komplett raus oder?
-		unsigned char lb: 1; 		// either approximation or collision (or both) light barriers triggered		
-		unsigned char closed: 1;	// door is closed		
-		unsigned char open: 1;		// door is open		
-		unsigned char _reserved: 5;	// fill remaining bits to prevent compiler "optimization" writing garbage into them
+		unsigned char offen: 1; 		// either approximation or collision (or both) light barriers triggered		
+		unsigned char oeff: 1;	// door is closed		
+		unsigned char geschlossen: 1;		// door is open	
+		unsigned char schliessen: 1; 		// either approximation or collision (or both) light barriers triggered			
+		unsigned char _reserved: 4;	// fill remaining bits to prevent compiler "optimization" writing garbage into them
 	} single;
 	unsigned char all;
 } ManuInputs_t;
@@ -30,31 +30,53 @@ typedef union {// TODO: define an input type for manual mode here
 static const struct { // TODO: insert FSM table for manual mode here
 	ManuState_t nextState;	
 	Actuators_t output;
-} autoTable[2][8] = {
-	/************************************************************
-	 *             |  inputs   ||                  |  outputs   *	
-	 *             |   | c |   ||                  |   | motor  *
-	 *             |   | l |   ||                  |   | c |    *
-	 *             | o | o |   ||                  | l | l | o  *
-	 *             | p | s |   ||                  | a | o | p  *
-	 * current     | e | e | l ||                  | m | s | e  *
-	 * state       | n | d | b || ->   next state, | p | e | n  * 
-	 ***********************************************************/	 	 	 	 
-	
-	// TODO: add table here
+} ManuTable[3][16] = {
+	{	//Ruhe
+		{ruhe,{{0,0,0}}/*Fehler*/},{ruhe,{{0,0,0}}/*Fehler*/}, {ruhe, {{0,0,0}}}, {ruhe, {{0,0,0}}},
+		{ruhe,{{0,0,0}}/*Fehler*/},{ruhe,{{0,0,0}}/*Fehler*/}, {oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}},
+		{ruhe, {{0,0,0}}}, {schliessen, {{1,1,0}}}, {ruhe,{{0,0,0}}/*Fehler*/},{ruhe,{{0,0,0}}/*Fehler*/},
+		{ruhe, {{0,0,0}}}, {ruhe, {{0,0,0}}}, {ruhe,{{0,0,0}}/*Fehler*/},{ruhe,{{0,0,0}}/*Fehler*/}
+	}	,
+	 //schliessen
+	{
+		{schliessen, {{1,1,0}}}, {schliessen, {{1,1,0}}}, {ruhe, {{0,0,0}}}, {ruhe, {{0,0,0}}},
+		{oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}},
+		{schliessen, {{1,1,0}}}, {schliessen, {{1,1,0}}}, {schliessen, {{0,0,0}}/*Fehler*/},{schliessen, {{0,0,0}}/*Fehler*/},
+		{ruhe, {{0,0,0}}}, {ruhe, {{0,0,0}}}, {schliessen, {{0,0,0}}/*Fehler*/},{schliessen, {{0,0,0}}/*Fehler*/}
+	}	,
+	//oeffnen
+	{
+		{oeffnen, {{0,0,1}}}, {oeffnen,{{0,0,0}}/*Fehler*/},{oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}},
+		{oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}}, {oeffnen, {{0,0,1}}},
+		{ruhe, {{0,0,0}}}, {oeffnen,{{0,0,0}}/*Fehler*/},{oeffnen,{{0,0,0}}/*Fehler*/},{oeffnen,{{0,0,0}}/*Fehler*/},
+		{ruhe, {{0,0,0}}}, {ruhe, {{0,0,0}}}, {oeffnen,{{0,0,0}}/*Fehler*/},{oeffnen,{{0,0,0}}/*Fehler*/}
+	}
 };
 
 
 static Actuators_t doManualCycle(Sensors_t sensors, int doInit) {
 	Actuators_t actuators;
+	ManuInputs_t inputs;
+	static ManuState_t state;
+	static int closeDelayCounter;
 		
 	if(doInit) {
-		// TODO: set initial state
-	} 	
+		closeDelayCounter = 0;
+		state = ruhe;
+	} 
 
 	// TODO: fill input structure
+	inputs.all = 0;
+	if(sensors.single.limit_open) inputs.single.offen = 1;
+	if(sensors.single.limit_closed) inputs.single.geschlossen = 1;
+	if(sensors.single.button_open)inputs.single.oeff=1;
+	if(sensors.single.button_close)inputs.single.schliessen=1;
 	
+	
+
 	// TODO: table based transition and output function	
+	actuators = ManuTable[state][inputs.all].output;
+	state = ManuTable[state][inputs.all].nextState;	
 
 		
 	return actuators; 	
@@ -69,7 +91,8 @@ static Actuators_t doManualCycle(Sensors_t sensors, int doInit) {
 
 // states
 typedef enum{
-	ruhe, oeffnen, schließen, fehler, // TODO: add states here
+	Bew_ohneLS, Lichtschranke, // TODO: add states here
+	Ruhe,
 	Auto_Initial // = Geschlossen
 } AutoState_t;
 
@@ -87,18 +110,15 @@ typedef union {
 static const struct { 
 	AutoState_t nextState;	
 	Actuators_t output;
-} autoTable[2]/*Anzahl Ausgangszustände*/ [8]/*Anzahl Eingangsvektoren*/ = {
-	/************************************************************
-	 *             |  inputs   ||                  |  outputs   *	
-	 *             |   | c |   ||                  |   | motor  *
-	 *             |   | l |   ||                  |   | c |    *
-	 *             | o | o |   ||                  | l | l | o  *
-	 *             | p | s |   ||                  | a | o | p  *
-	 * current     | e | e | l ||                  | m | s | e  *
-	 * state       | n | d | b || ->   next state, | p | e | n  * 
-	 ***********************************************************/	 	 	 	 
-	
-	// TODO: add table here
+} autoTable[4]/*Anzahl Zustände*/ [8]/*Anzahl Eingangsvektoren*/ = {
+	//Bew_ohneLS
+	{{Bew_ohneLS,{{1,1,0}}},		{Bew_ohneLS,{{0,0,1}}},			{Ruhe,{{0,0,0}}}, 			{Bew_ohneLS,{{0,0,0}}},/*Fehler*/		{Ruhe,{{0,0,0}}},		{Lichtschranke,{{0,0,0,}}},	{Bew_ohneLS,{{0,0,0}}},/*Fehler*/	{Bew_ohneLS,{{0,0,0}}}/*Fehler*/},
+	//Lichtschranke
+	{{Bew_ohneLS,{{0,0,1}}},		{Lichtschranke,{{0,0,1}}},		{Lichtschranke,{{0,0,1}}}, 	{Lichtschranke,{{0,0,0}}},/*Fehler*/	{Ruhe,{{0,0,0}}},		{Lichtschranke,{{0,0,0}}},	{Lichtschranke,{{0,0,0}}},/*Fehler*/{Lichtschranke,{{0,0,0}}}/*Fehler*/},
+	//Ruhe
+	{{Ruhe,{{0,0,0}}},/*Fehler*/ 	{Ruhe,{{0,0,0}}},/*Fehler*/		{Ruhe,{{0,0,0}}},			{Lichtschranke,{{0,0,1}}},				{Bew_ohneLS,{{1,1,0}}},	{Lichtschranke,{{0,0,0}}},	{Ruhe,{{0,0,0}}},/*Fehler*/			{Ruhe,{{0,0,0}}}/*Fehler*/},
+	//Auto_Initial
+	{{Bew_ohneLS,{{0,0,1}}},		{Lichtschranke,{{0,0,1}}},		{Ruhe,{{0,0,1/*hier*/}}},			{Lichtschranke,{{0,0,1}}},				{Ruhe,{{0,0,1/*hier*/}}},		{Lichtschranke,{{0,0,1/*hier*/}}},	{Auto_Initial,{{0,0,0}}},/*Fehler*/	{Auto_Initial,{{0,0,0}}}/*Fehler*/}
 };
 
 #define DOOR_CLOSE_DELAY	(5000 / FSM_INTERVAL_MS) // wait 5 seconds before closing the door
@@ -148,6 +168,7 @@ static unsigned char detectErrors(Sensors_t sensors, Actuators_t actuators) {
 	unsigned char mask = 0;
 	
 	if(sensors.single.limit_open && sensors.single.limit_closed) mask |= 0x1;  
+	if((sensors.single.limit_closed || sensors.single.limit_open) && (actuators.single.motor_close||actuators.single.motor_open)) mask |= 0x2; //Endlage, aber eine Motor ist an
 
 	// TODO: handle more errors here
 	
